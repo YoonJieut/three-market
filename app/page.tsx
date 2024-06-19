1c92bbb87689c62d78e611268421afc63ba88973
YoonJieut <qmdldos@gmail.com>
2024-06-20 02:50:05 +0900

    Chore : id 추가 및 안쓰는 코드 제거
    그러나 뭐가 문제일까.. glb를 가이드용으로 다시 받아서 해봐야겠다.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import dynamic from "next/dynamic";
-
 const CubeScene = dynamic(() => import("@/components/explosion/cube-scene"), {
   ssr: false,
 });
--- components/explosion/cube-scene.tsx
+++ components/explosion/cube-scene.tsx
@@ -7,14 +7,16 @@ import Cube from "./cube";
 
 export default function Scene() {
   return (
-    <Canvas gl={{ antialias: true }} dpr={[1, 1.5]}>
-      <directionalLight position={[-5, -5, -5]} intensity={2} />
-      <Suspense fallback={null}>
-        <Center>
-          {/* <Model /> 이렇게 모델을 넣어줌*/}
-          <Cube />
-        </Center>
-      </Suspense>
-    </Canvas>
+    <div id="canvas-container">
+      <Canvas gl={{ antialias: true }} dpr={[1, 1.5]}>
+        <directionalLight position={[-5, -5, -5]} intensity={2} />
+        <Suspense fallback={null}>
+          <Center>
+            {/* <Model /> 이렇게 모델을 넣어줌*/}
+            <Cube />
+          </Center>
+        </Suspense>
+      </Canvas>
+    </div>
   );
 }
