8e512001619a2e937a2b74bdf49757173c196255
YoonJieut <qmdldos@gmail.com>
2024-07-16 00:07:12 +0900

    test : three에 있는 컴포넌트들로 작성해보기

--- components/testModel/ModelView.tsx
+++ components/testModel/ModelView.tsx
@@ -1,4 +1,26 @@
-const ModelView = () => {
-  return <></>;
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
+import * as THREE from "three";
+
+const ModelView = ({ index }) => {
+  return (
+    <View
+      index={index}
+      className={`border-2 border-blue-500 ${index === 2} ? 'border-red-500`}
+    >
+      <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => {
+          setRotationState(controlRef.current.getAzimuthalAngle());
+        }}
+      />
+      {/* group 자리 */}
+    </View>
+  );
 };
 export default ModelView;
